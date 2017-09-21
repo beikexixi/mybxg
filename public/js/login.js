5d16dd74d9195c59980ec4e7cf59a0e9da1dc324
beikexixi <1264005131@qq.com>
2017-09-21 09:53:23 +0800

    文件模块化

---
+++ public/js/login.js
@@ -0,0 +1,29 @@
+define(['jquery','cookie'],function ($) {
+
+    $("#btn_login").on("click",function () {
+        $.ajax({
+            type: "post",
+            url: "/api/login",
+            data: $("#loginForm").serialize(),
+            dataType: "json",
+            success: function (data) {
+                console.log(data);
+                if(data.code == 200){
+                    // 登录成功
+                    // 保存用户信息
+                    $.cookie("per_info",JSON.stringify(data.result),{
+                        expires: 2,
+                        path: "/"
+                    });
+                    location.href = "/main/index";
+                }
+            },
+            error: function () {
+//                    location.href = "/main/index";
+                console.log("请求错误");
+            }
+
+        });
+        return false;
+    });
+});
